e7a204875f0ab08366d5363f0ba990a94d275841
imaginennn <imaginennn@users.noreply.github.com>
2025-05-19 19:09:55 +0200

    Add project files.

---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(1.01, double.MaxValue, ErrorMessage = "Amount is out of limits")]
+        public double Amount { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now; // Sets actual date when user stores the expense
+    }
+}
